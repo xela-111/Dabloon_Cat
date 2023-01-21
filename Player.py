04f86cfa8fccd1115e3a6638fe20c0a25c326437
bapshago <81396957+bapshago@users.noreply.github.com>
2023-01-21 11:10:45 -0500

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,24 +1,28 @@
 class Player:
-	"""docstring for ClassName"""
-	def __init__(self,  username, loc_x, loc_y, starting_dabloon,bag):
-		self.username = username
-		self.loc_x = loc_x
-		self.loc_y = loc_y
-		self.starting_dabloon = starting_dabloon
-		self.bag = bag
-	
-	def takedabloon(self, amount):
-		self.starting_dabloon = self.starting_dabloon-amount 
-		print("new amount" , self.starting_dabloon)
-	
-	def adddabloon(self,amount):
-		self.starting_dabloon = self.starting_dabloon+amount
-		print("new amount" , self.starting_dabloon)
+        """docstring for ClassName"""
+        def __init__(self,  username, loc_x, loc_y, starting_dabloon,bag):
+                self.username = username
+                self.loc_x = loc_x
+                self.loc_y = loc_y
+                self.starting_dabloon = starting_dabloon
+                self.bag = bag
+        
+        def takedabloon(self, amount):
+               if self.starting_dabloon >= amount: 
+                        self.starting_dabloon = self.starting_dabloon-amount
+                        print("Success!")
+                        return(amount)
+               else:
+                        print("Not enought dabloons")
+                        return (0)      
+        def adddabloon(self,amount):
+                self.starting_dabloon = self.starting_dabloon+amount
+                print("new amount" , self.starting_dabloon)
 
-	def addprize(self,prize):
-		self.bag.append(prize)
-		print(f"{prize} add to your bag!")
-		
+        def addprize(self,prize):
+                self.bag.append(prize)
+                print(f"{prize} add to your bag!")
+                
 
 
 
